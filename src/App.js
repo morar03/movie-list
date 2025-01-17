54ee876df6d214bd81ba14346ab5288f922e7948
morar03 <morarcristiantraian@gmail.com>
2025-01-17 17:43:14 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 import { GlobalStyle } from './GlobalStyle';
 import Header from './components/Headers';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
-const  App = () => {
-  return (
+const  App = () => (
+  <Router>
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:movieId" element={<Movie />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
       <GlobalStyle />
     </div>
+  </Router>
+
   );
-}
+
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,57 @@
 import React, { useState, useEffect } from "react";
 
-import API from "../API";
+import { useHomeFetch } from "./hooks/useHomeFetch";
+
+import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
+import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
+import Button from "./Button"; 
 
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
 
-    const fetchMovies = async (page, searchTerm = "") => {
-        try {
-            setError(false);
-            setLoading(true);
+    const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } = useHomeFetch();
+    console.log(state);
+    
 
-            const movies = await API.fetchMovies(searchTerm, page);
-
-            setState(prev => ({
-                ...movies,
-                results:
-                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }));
-        } catch (error) {
-            setError(true);
-        }
-        setLoading(false);
-    };
-
-  return <div>Home Page</div>;
+  if (error) return <div>Something went wrong...</div>
+  return (
+    <>
+    {state.total_results > 0 ? (
+        <HeroImage 
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+        title={state.results[0].original_title}
+        text={state.results[0].overview}
+        />
+        
+    ) : null}
+    <SearchBar setSearchTerm = {setSearchTerm} />
+    <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
+        
+    {state.total_results > 0 ? (state.results.map(movie => (
+            <Thumb
+            key={movie.id}
+            movieId={movie.id}
+            clickable
+            image={
+                movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }/>
+        ))
+      ) : null}
+    </Grid>
+    {loading && <Spinner />}
+    {state.page < state.total_pages && !loading && (
+        <Button text="Load More" callback={ () => setIsLoadingMore(true)}/>
+    )}
+    </>
+  );
 }
 
 export default Home;
